9ac4469931b97a061b3debff1eaabcf70503604c
DongYeon Kang <myjjue00@gmail.com>
2022-01-08 09:37:46 +0900

    Add ex02

---
+++ C_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donkang <donkang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/07 19:15:55 by donkang           #+#    #+#             */
+/*   Updated: 2022/01/07 19:46:18 by donkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwutsrqponmlkjihgfedcba", 26);
+}
