12b431fa1b2b824de76434d50d03ad4ee14f1038
DongYeon Kang <myjjue00@gmail.com>
2022-01-09 14:19:01 +0900

    Add ex08

--- C_00/ex08/ft_print_combn.c
+++ C_00/ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: donkang <donkang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 21:53:49 by donkang           #+#    #+#             */
-/*   Updated: 2022/01/08 21:29:18 by donkang          ###   ########.fr       */
+/*   Updated: 2022/01/09 13:17:41 by donkang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,29 @@
 
 char	g_data[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 char	g_flag[10] = {'0',};
+int	g_cnt = 0;
 
-void	combination(int r, int index, int depth)
+int	cnt_comb(int n)
+{
+	int	a, b;
+	int	x, y;
+	int	i;
+	
+	a = 1;
+	b = 1;
+	x = 10;
+	y = n;
+	i = -1;
+	while (++i < n)
+	{
+		a *= x--;
+		b *= y--;	
+	}
+
+	return (a/b);
+}
+
+void	combination(int r, int index, int depth, int cnt)
 {
 	int i;
 
@@ -23,10 +44,9 @@ void	combination(int r, int index, int depth)
 	{
 		i = -1;
 		while (++i < r)
-		{
 			write(1, &g_flag[i], 1);
-		}
-		write(1, "\n", 1);
+		if (++g_cnt < cnt)
+			write(1, ", ", 2);
 		return;
 	}
 	else
@@ -35,13 +55,15 @@ void	combination(int r, int index, int depth)
 		while(++i < 10)
 		{
 			g_flag[depth] = g_data[i];
-			combination(r, i + 1, depth + 1);
+			combination(r, i + 1, depth + 1, cnt);
 		}
 	}
 }
 void	ft_print_combn(int n)
-{
-	combination(n, 0, 0);
+{	
+	int	cnt;
+	cnt = cnt_comb(n);
+	combination(n, 0, 0, cnt);
 }
 int	main(void)
 {
