f0d7d5ed22d19f2705d6c9e0f9e9fc9ea5da737a
DongYeon Kang <myjjue00@gmail.com>
2022-01-17 12:52:34 +0900

    Add C_06

---
+++ C_06/ex00/ft_print_program_name.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    int i;
+    
+    i = 0;
+    if (ac >= 1)
+    {
+        while (av[0][i])
+            write(1, &av[0][i++], 1);
+        write(1, "\n", 1);
+    }
+    return (0);
+}
---
+++ C_06/ex03/ft_sort_params.c
@@ -0,0 +1,61 @@
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] > s2[i])
+			return (1);
+		else if (s1[i] < s2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_sort_int_tab(char **tab, int size)
+{
+	int	min_idx;
+	int	i;
+	int	j;
+	char	*tmp;
+
+	i = 0;
+	while (++i < size - 1)
+	{
+		min_idx = i;
+		j = i;
+		while (++j < size)
+		{
+			if (ft_strcmp(tab[j], tab[min_idx]) < 0)
+				min_idx = j;
+		}
+		tmp = tab[i];
+		tab[i] = tab[min_idx];
+		tab[min_idx] = tmp;
+	}
+}
+
+int main(int ac, char **av)
+{
+    int i;
+    int j;
+    
+    i = 1;
+    if (ac > 1)
+    {
+        ft_sort_int_tab(av, ac);
+        while (i < ac)
+        {
+            j = 0;
+            while (av[i][j])
+                write(1, &av[i][j++], 1);
+            write(1, "\n", 1);
+            i++;
+        }
+    }
+    return (0);
+}
