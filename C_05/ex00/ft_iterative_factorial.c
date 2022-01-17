933611697e7e346713b20f6a79a425470aadd782
DongYeon Kang <myjjue00@gmail.com>
2022-01-17 12:53:00 +0900

    Add ex00

---
+++ C_05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,25 @@
+int ft_iterative_factorial(int nb)
+{
+    int result;
+
+    result = 1;
+    if (nb <= 0)
+        return (0);
+    else 
+    {
+        while (nb > 0)
+        {
+            result *= nb;
+            nb--;
+        }
+        return (result);
+    }
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n = ft_iterative_factorial(3);
+    printf("%d", n);
+}
