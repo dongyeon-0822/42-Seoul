c4615e24775ef6ba8857cb4fd26bc059d6873b60
DongYeon Kang <myjjue00@gmail.com>
2022-01-19 11:56:06 +0900

    Add ex01

---
+++ C_07/ex01/ft_range.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donkang <donkang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 21:20:16 by donkang           #+#    #+#             */
+/*   Updated: 2022/01/18 22:02:42 by donkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*p;
+
+	if (min >= max)
+		return (0);
+	p = (int *)malloc(sizeof(int) * (max - min));
+	if (p == NULL)
+		return (0);
+	else
+	{
+		while (min < max)
+			p[i++] = min++;
+		return (p);
+	}
+}
