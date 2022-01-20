0d68a6dfc33e091f14503d1bea6430cc6ee24f03
DongYeon Kang <myjjue00@gmail.com>
2022-01-20 11:42:06 +0900

    Add ex00

---
+++ C_07/ex00/ft_strdup.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donkang <donkang@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/17 18:29:02 by donkang           #+#    #+#             */
+/*   Updated: 2022/01/17 18:29:04 by donkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	cnt;
+
+	cnt = 0;
+	while (str[cnt] != '\0')
+		cnt++;
+	return (cnt);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*pc;
+	int		len;
+
+	len = ft_strlen(str);
+	pc = (char *)malloc(sizeof(char) * (len + 1));
+	if (pc != NULL)
+		ft_strcpy(pc, str);
+	return (pc);
+}
