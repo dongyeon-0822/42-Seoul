ea216cae5457fcdb587981ddfc3d04fc48430e87
DongYeon Kang <myjjue00@gmail.com>
2022-01-22 13:58:42 +0900

    Add ...

---
+++ C_07/ex04/convert_base_2.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	find(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	negative;
+	int	i;
+	int	result;
+	int	index;
+
+	negative = 1;
+	i = 0;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] && (str[i] == '+' || str[i] == '-'))
+	{
+		if (str[i] == '-')
+			negative *= -1;
+		i++;
+	}
+	index = find(base, str[i]);
+	while (str[i] && index >= 0)
+	{
+		result = result * ft_strlen(base) + index;
+		index = find(base, str[++i]);
+	}
+	return (result * negative);
+}
