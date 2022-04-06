a5ef0f5d2d7896771f1b5fcf6107dfab5d36f571
myjjue00 <myjjue00@gmail.com>
2022-04-06 20:52:01 +0900

    Add ft_strlcat.c

---
+++ Libft/ft_strlcat.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donkang <donkang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/06 20:42:01 by donkang           #+#    #+#             */
+/*   Updated: 2022/04/06 20:51:07 by donkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	destsize;
+	unsigned int	srcsize;
+	unsigned int	idx;
+
+	destsize = ft_strlen(dest);
+	srcsize = ft_strlen(src);
+	idx = 0;
+	if (destsize >= size)
+		return (srcsize + size);
+	while (src[idx] && destsize + idx + 1 < size)
+	{
+		dest[destsize + idx] = src[idx];
+		idx++;
+	}
+	dest[destsize + idx] = '\0';
+	return (srcsize + destsize);
+}
