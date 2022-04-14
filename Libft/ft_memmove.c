ce2cf7bbd111e570b56059ec1b5b7823cfdea642
myjjue00 <myjjue00@gmail.com>
2022-04-14 23:47:43 +0900

    Add ft_memmove.c

---
+++ Libft/ft_memmove.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donkang <donkang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 23:37:26 by donkang           #+#    #+#             */
+/*   Updated: 2022/04/14 23:47:13 by donkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dest, const void *src, size_t num)
+{
+	size_t	i;
+
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	if (dest < src)
+	{
+		i = 0;
+		while (i < num)
+		{
+			((unsigned char*)dest)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
+	}
+	else
+	{
+		i = num;
+		while (i)
+		{
+			((unsigned char *)dest[i - 1] = ((unsigned char *)src)[i - 1];
+			 i--;
+		}
+	}
+	return (dest);
+}
