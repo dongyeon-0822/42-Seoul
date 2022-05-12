4d5f259a115e7c4a7165b22e86570d8b573aaece
myjjue00 <myjjue00@gmail.com>
2022-05-12 23:44:19 +0900

    Modify ft_strncmp.c

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -14,13 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t num)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	i = 0;
-	while ((s1[i] || s2[i]) && i < num)
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (i < num)
 	{
-		if (s1[i] > s2[i] || s1[i] < s2[i])
-			return (s1[i] - s2[i]);
+		if (str1[i] != str2[i] || str1[i] == 0 || str2[i] == 0)
+			return (str1[i] - str2[i]);
 		i++;
 	}
 	return (0);
