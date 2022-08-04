1eb39371380550580954c5a8836fffcd83476710
myjjue00 <myjjue00@gmail.com>
2022-08-04 23:14:49 +0900

    Create get_next_line.h

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donkang <donkang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/04 20:53:10 by donkang           #+#    #+#             */
+/*   Updated: 2022/08/04 20:53:13 by donkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5000
+#endif
+
+char		*get_next_line(int fd);
+char		*get_line(char	*backup);
+char		*backup_next(char	*backup);
+char		*read_and_store(int fd, char *backup);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int searchedChar);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t elementCount, size_t elementSize);
+size_t	ft_strlen(const char *theString);
+
+#endif
